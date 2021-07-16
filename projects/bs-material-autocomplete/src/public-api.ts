add8675e965de8e32fa65d7867e848bca321b6ef
Brian Smith <smithbrianscott23@gmail.com>
2021-07-16 09:39:44 -0400

    removed service

--- projects/bs-material-autocomplete/src/public-api.ts
+++ projects/bs-material-autocomplete/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of bs-material-autocomplete
  */
 
-export * from './lib/bs-material-autocomplete.service';
 export * from './lib/bs-material-autocomplete.component';
 export * from './lib/bs-material-autocomplete.module';
